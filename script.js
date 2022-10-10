b377bce82fc09c8ab4832b51b234ee127bf450fe
Oguz <aka@pop-os.localdomain>
2022-10-11 01:16:09 +0300

    failed many implementations. i will re-challenge it tomorrow

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 'use strict';
-// Play audio with key press
 const validKeys = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o'];
 
+// transition
+
+// Play audio with key press
 document.addEventListener('keydown', function (e) {
   if (validKeys.includes(`${e.key}`)) {
-    const whichKey = e.key;
-    const whichNotation = document.querySelector(
-      `.${whichKey}-key`
-    ).textContent;
+    // key and notation variables
+    const whichNotation = document.querySelector(`.${e.key}-key`).textContent;
+    document.querySelector(`.${whichNotation}-main`).classList.add('playing');
+    // Audio Play
     const audio = new Audio(`/sounds/${whichNotation}.wav`);
     audio.play();
   }
