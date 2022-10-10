9dcb70b6cc4cea9ce75e642976e739fd189e50bd
Oguz <aka@pop-os.localdomain>
2022-10-11 00:03:42 +0300

    implemented audio effects.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+// Play audio with key press
+const validKeys = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o'];
+
+document.addEventListener('keydown', function (e) {
+  if (validKeys.includes(`${e.key}`)) {
+    const whichKey = e.key;
+    const whichNotation = document.querySelector(
+      `.${whichKey}-key`
+    ).textContent;
+    const audio = new Audio(`/sounds/${whichNotation}.wav`);
+    audio.play();
+  }
+});
